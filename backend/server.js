e6be7a5bba0b775fc19d5b706dd52ab6a6dfd465
shrashti-19 <shrashtidwivedi91@gmail.com>
2025-05-29 01:26:34 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,11 @@ app.get('/test', (req, res) => {
   res.send('🧪 Test route working');
 });
 
+app.get('/', (req, res) => {
+  res.redirect('http://127.0.0.1:5501/frontend/index.html');
+});
+
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
